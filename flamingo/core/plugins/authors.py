13d27a6e39aa3a8fe1b95fc91ef6fadd0ac1b7a6
Florian Scherf <f.scherf@pengutronix.de>
2018-12-06 00:48:58 +0100

    core: plugins: authors: fix content_keys; sort author names
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- flamingo/core/plugins/authors.py
+++ flamingo/core/plugins/authors.py
@@ -13,25 +13,17 @@ class Authors:
                 content['authors'] = [a.strip()
                                       for a in content['authors'].split(',')]
 
-        authors = sum(context.contents.values('authors'), [])
+        authors = sorted(
+            list(set(sum(context.contents.values('authors'), []))))
 
         # gen author pages
         for author in authors:
             output = os.path.join('authors/{}.html'.format(author))
 
             context.contents.add(**{
-                content_key: '_author/{}'.format(author),
+                content_key: '_authors/{}'.format(author),
                 'output': output,
                 'url': '/' + output,
                 'author': author,
                 'template': 'author.html',
             })
-
-        # gen author list
-        context.contents.add(**{
-            content_key: '_authors',
-            'output': 'authors.html',
-            'url': '/authors.html',
-            'authors': authors,
-            'template': 'authors.html',
-        })
