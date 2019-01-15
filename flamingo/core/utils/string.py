7b1fc8ef4a1d3225d282686ca52a92c426a85a10
Florian Scherf <f.scherf@pengutronix.de>
2019-01-15 12:36:17 +0100

    core: utils: add library for string manipulation
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

---
+++ flamingo/core/utils/string.py
@@ -0,0 +1,31 @@
+import re
+
+CLEAN_RE = re.compile(r'[^a-z0-9äöüßø]')
+WHITESPACE_RE = re.compile(r'\s+')
+
+ESCAPE_TABLE = [
+    ('ä', 'ae'),
+    ('ö', 'oe'),
+    ('ü', 'ue'),
+    ('ø', 'oe'),
+    ('ß', 'ss'),
+]
+
+
+def slugify(string):
+    string = string.lower()
+    string = CLEAN_RE.sub(' ', string)
+    string = string.strip()
+    string = WHITESPACE_RE.sub('-', string)
+
+    for a, b in ESCAPE_TABLE:
+        string = string.replace(a, b)
+
+    return string
+
+
+def split(string, delimiter=','):
+    string = WHITESPACE_RE.sub(' ', string.strip())
+    strings = [i for i in string.split(delimiter) if i]
+
+    return strings
