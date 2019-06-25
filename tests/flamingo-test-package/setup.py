f045c7142d9bcc19a3a32331951d5f6e536bb184
Florian Scherf <f.scherf@pengutronix.de>
2019-06-25 18:06:48 +0200

    tests: setup flamingo_tests
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

---
+++ tests/flamingo-test-package/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='flamingo-test-package',
+    version='0.0',
+    packages=find_packages(),
+)
