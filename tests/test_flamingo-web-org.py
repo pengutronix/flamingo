276a88eac40009c385509b10c0190e17a52fc24e
Florian Scherf <f.scherf@pengutronix.de>
2019-07-03 17:41:59 +0200

    tests: fix flamingo-web.org test for travis
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- tests/test_flamingo-web-org.py
+++ tests/test_flamingo-web-org.py
@@ -1,7 +1,11 @@
 def test_flamingo_web_org(run):
+    import sys
     import os
 
-    returncode, output = run('make clean html', cwd='flamingo-web.org')
+    returncode, output = run(
+        'make clean -e PYTHON={} html'.format(sys.executable),
+        cwd='flamingo-web.org',
+    )
 
     assert returncode == 0
     assert os.path.exists(os.path.join('flamingo-web.org/output/index.html'))
