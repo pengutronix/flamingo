1a4e9170a3ec0232f106b5e70e33bd6a02acc946
Florian Scherf <f.scherf@pengutronix.de>
2019-08-03 23:39:28 +0200

    project templates: bootstrap4: port to v0.10
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- flamingo/project_templates/bootstrap4/plugins/example.py
+++ flamingo/project_templates/bootstrap4/plugins/example.py
@@ -2,13 +2,13 @@ class ExamplePlugin:
     def parser_setup(self, context):
         pass
 
-    def content_parsed(self, context, content):
+    def content_parsed(self, context):
         pass
 
     def contents_parsed(self, context):
         pass
 
-    def templating_engine_setup(self, context, templating_engine):
+    def templating_engine_setup(self, context, templating_engine_setup):
         pass
 
     def context_setup(self, context):
