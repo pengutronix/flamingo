f53e22984f9c88f44f51e1193d1eec283d527b7c
Florian Scherf <f.scherf@pengutronix.de>
2019-08-16 16:12:10 +0200

    plugins: Feeds: add error managment
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- flamingo/plugins/feeds.py
+++ flamingo/plugins/feeds.py
@@ -1,4 +1,8 @@
+import logging
+
 from feedgen.feed import FeedGenerator
+
+logger = logging.getLogger('flamingo.plugins.Feeds')
 
 
 class Feeds:
@@ -7,60 +11,91 @@ class Feeds:
         FEEDS = getattr(context.settings, 'FEEDS', [])
 
         for feed_config in FEEDS:
-            content = {
-                'type': 'feed',
-                'feed_type': feed_config['type'],
-                'output': feed_config['output'],
-                'url': '/' + feed_config['output'],
-            }
+            try:
+                content = {
+                    'type': 'feed',
+                    'feed_type': feed_config['type'],
+                    'output': feed_config['output'],
+                    'url': '/' + feed_config['output'],
+                }
 
-            if 'lang' in feed_config:
-                content['lang'] = feed_config['lang']
+                if 'lang' in feed_config:
+                    content['lang'] = feed_config['lang']
 
-            fg = FeedGenerator()
+                fg = FeedGenerator()
 
-            fg.id(feed_config['id'])
-            fg.title(feed_config['title'])
+                fg.id(feed_config['id'])
+                fg.title(feed_config['title'])
 
-            for i in feed_config['contents'](context):
-                fe = fg.add_entry()
+                for i in feed_config['contents'](context):
+                    fe = fg.add_entry()
 
-                fe.title(i['content_title'])
-                fe.content(i['content_body'], type='html')
+                    # setup required entry attributes
+                    fe_title = i['content_title']
 
-                fe.link({
-                    'href': '{}{}'.format(FEEDS_DOMAIN, i['url']),
-                    'rel': 'alternate'
-                })
+                    fe_link = {
+                        'href': '{}{}'.format(FEEDS_DOMAIN, i['url']),
+                        'rel': 'alternate'
+                    }
 
-                if 'entry-id' in feed_config:
-                    fe.id(feed_config['entry-id'](i))
+                    if 'entry-id' in feed_config:
+                        fe_id = feed_config['entry-id'](i)
 
-                else:
-                    fe.id(i['output'])
+                    else:
+                        fe_id = i['output']
 
-                if 'updated' in feed_config:
-                    updated = feed_config['updated'](i)
+                    if 'updated' in feed_config:
+                        fe_updated = feed_config['updated'](i)
 
-                    if updated:
-                        fe.updated(updated)
+                    else:
+                        fe_updated = ''
 
-                if i['authors']:
-                    for author in i['authors']:
-                        fe.author({
-                            'name': author,
-                        })
+                    # check entry attributes
+                    missing_attributes = []
 
-                if i['summary']:
-                    fe.summary(str(i['summary']))
+                    if not fe_id:
+                        missing_attributes.append('id')
 
-                if i['title']:
-                    fe.title(i['title'])
+                    if not fe_title:
+                        missing_attributes.append('title')
 
-            if feed_config['type'] == 'atom':
-                content['content_body'] = fg.atom_str().decode()
+                    if not fe_updated:
+                        missing_attributes.append('updated')
 
-            elif feed_config['type'] == 'rss':
-                content['content_body'] = fg.rss_str().decode()
+                    if missing_attributes:
+                        logger.error('%s is missing attributes: %s',
+                                     i['path'] or i,
+                                     ', '.join(missing_attributes))
 
-            context.contents.add(**content)
+                        return
+
+                    # optional attributes
+                    fe.id(fe_id)
+                    fe.title(fe_title)
+                    fe.updated(fe_updated)
+                    fe.link(fe_link)
+
+                    if i['content_body']:
+                        fe.content(i['content_body'], type='html')
+
+                    if i['authors']:
+                        for author in i['authors']:
+                            fe.author({
+                                'name': author,
+                            })
+
+                    if i['summary']:
+                        fe.summary(str(i['summary']))
+
+                # generate output
+                if feed_config['type'] == 'atom':
+                    content['content_body'] = fg.atom_str().decode()
+
+                elif feed_config['type'] == 'rss':
+                    content['content_body'] = fg.rss_str().decode()
+
+                context.contents.add(**content)
+
+            except Exception:
+                logger.error("feed '%s' setup failed", feed_config['id'],
+                             exc_info=True)
