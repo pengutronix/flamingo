5c3a9717dabb80bb2107656f5897f015a3ba6eb1
Florian Scherf <f.scherf@pengutronix.de>
2019-10-28 22:40:56 +0100

    tests: docs: skip unused python versions
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- tests/test_flamingo-web-org.py
+++ tests/test_flamingo-web-org.py
@@ -1,5 +1,10 @@
 def test_flamingo_web_org(run):
+    import pytest
+    import sys
     import os
+
+    if (sys.version_info.major, sys.version_info.minor, ) != (3, 6, ):
+        pytest.skip('flamingo-web.org uses python3.6')
 
     returncode, output = run('make clean html', cwd='flamingo-web.org',
                              clean_env=True)
