a11e40ff3a40c47f95196880f8ce88ea0faf5fd9
Florian Scherf <f.scherf@pengutronix.de>
2020-01-23 09:21:41 +0100

    release v0.12
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- flamingo/__init__.py
+++ flamingo/__init__.py
@@ -4,7 +4,7 @@ from flamingo.core.plugins.plugin_manager import hook  # NOQA
 
 _dirname = os.path.dirname(__file__)
 
-VERSION = (0, 11)
+VERSION = (0, 12)
 VERSION_STRING = '.'.join([str(i) for i in VERSION])
 THEME_ROOT = os.path.join(_dirname, 'theme')
 PROJECT_TEMPLATES_ROOT = os.path.join(_dirname, 'project_templates')
