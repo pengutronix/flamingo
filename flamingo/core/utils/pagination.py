455a72508cfb770cb640e4939c84dd33e8801780
Florian Scherf <f.scherf@pengutronix.de>
2020-05-16 17:09:44 +0200

    core: utils: pagination: pep8 fixes
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- flamingo/core/utils/pagination.py
+++ flamingo/core/utils/pagination.py
@@ -1,23 +1,24 @@
-def paginate(l, context):
+def paginate(objects, context):
     """
     yields: page, page_index, total_pages
 
     """
 
     DEFAULT_PAGINATION = getattr(context.settings, 'DEFAULT_PAGINATION')
-    l_count = len(l)
+    object_count = len(objects)
 
-    if l_count > DEFAULT_PAGINATION:
+    if object_count > DEFAULT_PAGINATION:
         total_pages = (
-            (l_count - (l_count % DEFAULT_PAGINATION)) // DEFAULT_PAGINATION
-        ) + int((l_count % DEFAULT_PAGINATION) > 0)
+            (object_count - (object_count %
+                             DEFAULT_PAGINATION)) // DEFAULT_PAGINATION
+        ) + int((object_count % DEFAULT_PAGINATION) > 0)
 
     else:
         total_pages = 1
 
     for index in range(total_pages):
         yield (
-            l[index*DEFAULT_PAGINATION:(index+1)*DEFAULT_PAGINATION],
+            objects[index*DEFAULT_PAGINATION:(index+1)*DEFAULT_PAGINATION],
             index + 1,
             total_pages,
         )
