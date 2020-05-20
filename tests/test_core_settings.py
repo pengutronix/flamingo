d91e9ac2ee24352a3368dc19a19f33aa99c2389c
Florian Scherf <f.scherf@pengutronix.de>
2020-05-20 18:06:18 +0200

    tests: core: settings: add overlay tests
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- tests/test_core_settings.py
+++ tests/test_core_settings.py
@@ -1,4 +1,4 @@
-def test_settings_overlays(run):
+def test_overlaying_settings(run):
     from tempfile import TemporaryDirectory
     import json
     import os
@@ -27,3 +27,37 @@ def test_settings_overlays(run):
         assert args['settings']['a'] == 'settings'
         assert args['settings']['b'] == 'production'
         assert args['settings']['PLUGINS'] == ['foo']
+
+
+def test_settings_reset():
+    from flamingo.core.settings import Settings
+
+    settings = Settings()
+
+    settings.test_int = 10
+    settings.test_list = ['a', 'b', 'c']
+    settings.test_dict = {'a': 'a', 'b': 'b', 'c': 'c'}
+
+    settings.overlay_enable()
+
+    settings.test_int = 20
+    settings.test_list.append('d')
+    settings.test_dict['d'] = 'd'
+
+    assert settings.test_int == 20
+
+    assert settings.test_list[0] == 'a'
+    assert settings.test_list[1] == 'b'
+    assert settings.test_list[2] == 'c'
+    assert settings.test_list[3] == 'd'
+
+    assert settings.test_dict['a'] == 'a'
+    assert settings.test_dict['b'] == 'b'
+    assert settings.test_dict['c'] == 'c'
+    assert settings.test_dict['d'] == 'd'
+
+    settings.overlay_reset()
+
+    assert settings.test_int == 10
+    assert settings.test_list == ['a', 'b', 'c']
+    assert settings.test_dict == {'a': 'a', 'b': 'b', 'c': 'c'}
