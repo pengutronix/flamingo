3b27559361df589b9bd75a7717f906a7fb720f5a
Florian Scherf <f.scherf@pengutronix.de>
2021-01-17 12:57:56 +0100

    core: utils: html: rename extract_section_by_title to get_section_to_title
    This patch also brings support for multiple heading tags and brings some
    code clean-up
    
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- flamingo/core/utils/html.py
+++ flamingo/core/utils/html.py
@@ -1,15 +1,19 @@
+import re
+
 from bs4 import BeautifulSoup
 
 from flamingo.core.plugins.media import add_media
+
+HEADING_RE = re.compile('^h[1-6]$')
 
 
 class TitleNotFoundError(Exception):
     pass
 
 
-def extract_section_by_title(html, title, tag='h2'):
+def get_section_by_title(html, title, tag=None):
     soup = BeautifulSoup(html, 'html.parser')
-    html = soup.find(name=tag, text=title)
+    html = soup.find(name=tag or HEADING_RE, text=title)
 
     if not html:
         raise TitleNotFoundError
@@ -25,6 +29,7 @@ def extract_title(html):
 
     if h1:
         h1.extract()
+
         return h1.text
 
     return ''
