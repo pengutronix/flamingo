c76fb10d09c1ea057befc4c46528512df4a28a51
Jan Heinemeyer <j.heinemeyer@pengutronix.de>
2026-08-11 07:25:43 +0200

    plugins: add 'static_file' Jinja2 filter to Static plugin
    Static files are often cached. Changes to static files are then not
    immediately propagated to the clients resulting in broken pages until
    the cache expires or the user force-reloads the page.
    
    Since the HTTP cache is tied to the URL one way to invalidate cache is
    to change the URL when the file contents change.
    
    Add a Jinja2 filter 'static_file' that will generate a URL with the
    md5sum of the file appended as a query parameter. Appending a query
    parameter (the part after the '?') is not invasive.
    
    Using the 'static_file' filter is optional.
    
    Signed-off-by: Jan Heinemeyer <j.heinemeyer@pengutronix.de>

--- flamingo/core/plugins/static.py
+++ flamingo/core/plugins/static.py
@@ -1,7 +1,56 @@
+import hashlib
 import os
 
 
 class Static:
+    def templating_engine_setup(self, context, templating_engine):
+        """Add a Jinja2 filter that appends md5sum to static files as a query parameter
+
+        Adding the checksum as a query parameter invalidates any HTTP caching when the file
+        contents change.
+
+        Usage Example:
+        <link rel="stylesheet" href="{{ static_file('theme/static/css/style.css') }}" type="text/css" />
+
+        Will be rendered as
+        <link rel="stylesheet" href="/static/css/style.css?<md5sum>" type="text/css" />
+        """
+
+        self.static_files_map = {}
+
+        for static_dir in context.templating_engine.find_static_dirs():
+            for root, _dirs, files in os.walk(static_dir):
+                for f in files:
+                    source = os.path.join(root, f)
+
+                    href = os.path.join(
+                        "/",
+                        os.path.relpath(
+                            context.settings.STATIC_ROOT,
+                            context.settings.OUTPUT_ROOT,
+                        ),
+                        os.path.relpath(root, static_dir),
+                        f,
+                    )
+
+                    with open(source, "rb") as file:
+                        md5 = hashlib.file_digest(file, hashlib.md5).hexdigest()
+
+                    self.static_files_map[source] = {
+                        "href": href,
+                        "md5": md5,
+                    }
+
+        def static_file(source: str):
+            return "?".join(
+                (
+                    self.static_files_map[source]["href"],
+                    self.static_files_map[source]["md5"],
+                )
+            )
+
+        templating_engine.env.globals["static_file"] = static_file
+
     def post_build(self, context):
         if context.settings.CONTENT_PATHS:
             return
