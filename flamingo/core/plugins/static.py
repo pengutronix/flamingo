2e32e387b5e30b1f6d02cacb7d535dc39c8a4aa7
Jan Heinemeyer <j.heinemeyer@pengutronix.de>
2026-08-11 07:39:09 +0200

    plugins: add helper method to Static plugin
    Put the "iterate over all static files" part into a helper method to
    simplify the plugin.
    
    Signed-off-by: Jan Heinemeyer <j.heinemeyer@pengutronix.de>

--- flamingo/core/plugins/static.py
+++ flamingo/core/plugins/static.py
@@ -3,6 +3,12 @@ import os
 
 
 class Static:
+    def _iter_static_files(self, context):
+        for static_dir in context.templating_engine.find_static_dirs():
+            for dirpath, _dirnames, filenames in os.walk(static_dir):
+                for filename in filenames:
+                    yield (static_dir, dirpath, filename)
+
     def templating_engine_setup(self, context, templating_engine):
         """Add a Jinja2 filter that appends md5sum to static files as a query parameter
 
@@ -18,28 +24,26 @@ class Static:
 
         self.static_files_map = {}
 
-        for static_dir in context.templating_engine.find_static_dirs():
-            for root, _dirs, files in os.walk(static_dir):
-                for f in files:
-                    source = os.path.join(root, f)
+        for static_dir, dirpath, filename in self._iter_static_files(context):
+            source = os.path.join(dirpath, filename)
 
-                    href = os.path.join(
-                        "/",
-                        os.path.relpath(
-                            context.settings.STATIC_ROOT,
-                            context.settings.OUTPUT_ROOT,
-                        ),
-                        os.path.relpath(root, static_dir),
-                        f,
-                    )
+            href = os.path.join(
+                "/",
+                os.path.relpath(
+                    context.settings.STATIC_ROOT,
+                    context.settings.OUTPUT_ROOT,
+                ),
+                os.path.relpath(dirpath, static_dir),
+                filename,
+            )
 
-                    with open(source, "rb") as file:
-                        md5 = hashlib.file_digest(file, hashlib.md5).hexdigest()
+            with open(source, "rb") as file:
+                md5 = hashlib.file_digest(file, hashlib.md5).hexdigest()
 
-                    self.static_files_map[source] = {
-                        "href": href,
-                        "md5": md5,
-                    }
+            self.static_files_map[source] = {
+                "href": href,
+                "md5": md5,
+            }
 
         def static_file(source: str):
             return "?".join(
@@ -55,15 +59,11 @@ class Static:
         if context.settings.CONTENT_PATHS:
             return
 
-        for static_dir in context.templating_engine.find_static_dirs():
-            for root, _dirs, files in os.walk(static_dir):
-                for f in files:
-                    source = os.path.join(root, f)
-
-                    destination = os.path.join(
-                        context.settings.STATIC_ROOT,
-                        os.path.relpath(root, static_dir),
-                        f,
-                    )
-
-                    context.cp(source=source, destination=destination)
+        for static_dir, dirpath, filename in self._iter_static_files(context):
+            source = os.path.join(dirpath, filename)
+            destination = os.path.join(
+                context.settings.STATIC_ROOT,
+                os.path.relpath(dirpath, static_dir),
+                filename,
+            )
+            context.cp(source=source, destination=destination)
